34e87d2855f2c1ccf0ad38f0f36ab401a2e0ffbe
Eduard Nistru <edi@wearewip.com>
2019-03-06 11:51:32 +0200

    Donation buttons and image for mobile

--- src/components/survey/index.js
+++ src/components/survey/index.js
@@ -1,15 +1,43 @@
 import { h, Component } from 'preact';
 import style from './style';
-import { Text } from 'preact-i18n';
+import { Text, withText } from 'preact-i18n';
 
+@withText({
+  text: <Text id="donate.subtitle" />
+})
 export default class Survey extends Component {
-  constructor() {
-    super();
+    state = {
+      step: 1,
+      typingText: '',
+      showCursor: true
+    }
 
-    this.state = {}
+    delay = 0;
+    initialDelay = 1000;
+    letterDelay = 45;
+
+  handleClick(button) {
+    this.setState({ 
+      step: this.state.step + button.increment
+    })
+    if (button.link) {
+      window.location = button.link;
+    }
   }
 
-  render() {
+  componentDidMount() {
+    let delay = 0;
+
+    for (let i = 1; i <= this.props.text.length; i++) {
+      delay += Math.round(Math.random() * this.letterDelay / 2) + this.letterDelay;
+
+      if (i === 1 || this.props.text[i - 2] === '.' || this.props.text[i - 2] === '?') delay += this.initialDelay;
+
+      setTimeout(() => this.setState({ typingText: this.props.text.substring(0, i), showCursor: i !== this.props.text.length }), delay);
+    }
+  }
+
+  render({ steps }, { typingText }) {
     return (
       <div className={style.container}>
         <div className={style.container__box}>
@@ -18,13 +46,19 @@ export default class Survey extends Component {
               <Text id="donate.title" />
             </h1>
             <h2>
-              <Text id="donate.header" />
+              {typingText}
             </h2>
           </div>
           <div className={style.container__buttons}>
-            <button style={style.button}>
-              <Text id="donate.question" />
-            </button>
+          {
+            steps[this.state.step].buttons.map(button => {
+              return (
+                <button style={style.button} onClick={() => this.handleClick(button)}>
+                  {button.title}
+                </button>
+              )
+            })
+          }
           </div>
         </div>
       </div>
--- src/routes/donate/index.js
+++ src/routes/donate/index.js
@@ -3,12 +3,84 @@ import YouTube from 'react-youtube';
 import { Text } from 'preact-i18n';
 import Survey from '../../components/survey';
 import style from './style';
-import VolumeHighIcon from 'mdi-preact/VolumeHighIcon';
-import VolumeMuteIcon from 'mdi-preact/VolumeMuteIcon';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 const videoId = 'EoG4Jw5Vs0U';
 window.YTConfig = {
   host: 'https://www.youtube.com'
+}
+
+const steps = {
+  1: {
+    buttons: [
+      { 
+        title: <Text id="donate.buttons.donate" />,
+        link: '',
+        increment: 1 
+      }
+    ]
+  },
+  2: {
+    buttons: [
+      { 
+        title: <Text id="donate.buttons.monthlyDonation" />,
+        link: '',
+        increment: 2 
+      },
+      { 
+        title: <Text id="donate.buttons.singleDonation" />,
+        link: '',
+        increment: 1 
+      }
+    ]
+  },
+  3: {
+    buttons: [
+      { 
+        title: 'Go Fund Me',
+        link: 'https://www.gofundme.com/naturaumanafilm',
+        increment: 0
+      },
+      { 
+        title: 'PayPal',
+        link: 'https://www.paypal.me/naturaumanafilm',
+        increment: 0
+      }
+      // { title: 'Revolut', increment: 0 }
+    ]
+  },
+  4: {
+    buttons: [
+      { 
+        title: <Text id="donate.buttons.oneEuro" />,
+        link: '',
+        increment: 1
+      },
+      { 
+        title: <Text id="donate.buttons.moreEuros" />,
+        link: '',
+        increment: 2
+      }
+    ]
+  },
+  5: {
+    buttons: [
+      { 
+        title: 'teaming',
+        link: 'https://www.teaming.net/naturaumanafilm',
+        increment: 0
+      }
+    ]
+  },
+  6: {
+    buttons: [
+      { 
+        title: 'patreon',
+        link: 'https://www.patreon.com/naturaumanafilm',
+        increment: 0 
+      }
+    ]
+  }
 }
 
 export default class Donate extends Component {
@@ -29,7 +101,7 @@ export default class Donate extends Component {
 
   _onReady(event) {
     // access to player in all event handlers via event.target
-    // event.target.mute();
+    event.target.mute();
   }
 
   _onEnd(event) {
@@ -37,33 +109,38 @@ export default class Donate extends Component {
   }
 
   render() {
-    const { mute } = this.state;
     const videoOptions = {
       playerVars: {
         autoplay: true,
         controls: false,
         rel: false,
         showinfo: false,
-        volume: true,
-        mute: mute
+        volume: true
       }
     };
 
     return(
       <div className={style.video__background}>
-        <div className={style.video__foreground}>
-          <YouTube
-            videoId={videoId}
-            opts={videoOptions}
-            className="video-iframe"
-            onReady={this._onReady}
-            onEnd={this._onEnd}
-          />
-        </div>
+        <BrowserView>
+          <div className={style.video__foreground}>
+            <YouTube
+              videoId={videoId}
+              opts={videoOptions}
+              className="video-iframe"
+              onReady={this._onReady}
+              onEnd={this._onEnd}
+            />
+          </div>
+        </BrowserView>
+        <MobileView>
+          <div className={style.image__foreground}>
+            <img src="/assets/images/cover.jpg" alt="human nature" />
+          </div>
+        </MobileView>
         <div className={style.video__volume} onClick={() => this.handleVolume()}>
-          {this.state.mute ? <VolumeMuteIcon color="#fff"/> : <VolumeHighIcon color="#fff"/>}
+          {/* {mute ? <VolumeMuteIcon color="#fff"/> : <VolumeHighIcon color="#fff"/>} */}
         </div>
-        <Survey />
+        <Survey steps={steps} />
       </div>
     )
   }
