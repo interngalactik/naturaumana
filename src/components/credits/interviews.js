93bd9a695250c2edb8e25ef25c72b5e1705ca769
Eduard <eduard.nistru@gmail.com>
2020-02-03 16:19:15 +0200

    interviewees fontsize for mobile

--- src/components/credits/interviews.js
+++ src/components/credits/interviews.js
@@ -3,6 +3,7 @@ import style from './style';
 
 import YouTube from 'react-youtube';
 import Modal from '../modal';
+import { isMobile } from 'react-device-detect';
 
 const interviews = [
     {
@@ -11,21 +12,30 @@ const interviews = [
                 name: 'Starring',
                 text: '',
                 videoId: '',
-                fontSize: '3vw',
+                fontSize: {
+                    browser: '3vw',
+                    mobile: '5vw'
+                },
                 word: true
             },
             {
                 name: 'Paula Seling',
                 text: '',
                 videoId: '',
-                fontSize: '6vw',
+                fontSize: {
+                    browser: '6vw',
+                    mobile: '10vw'
+                },
                 word: false
             },
             {
                 name: 'Razvan Eremia (Deliric)',
                 text: '',
                 videoId: '',
-                fontSize: '6vw',
+                fontSize: {
+                    browser: '6vw',
+                    mobile: '10vw'
+                },
                 word: false
             }
         ]
@@ -36,28 +46,40 @@ const interviews = [
                 name: 'Remus Cernea',
                 text: ' Remus Cernea ne vorbeşte despre schimbare şi despre empatia faţă de cei care au alte opinii.',
                 videoId: 'CfVidVPVSmE',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '8vw'
+                },
                 word: false
             },
             {
                 name: 'Andrei Rosu',
                 text: 'Andrei Rosu, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 videoId: 'mvk7o09s2kE',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '8vw'
+                },
                 word: false
             },
             {
                 name: 'Marius Mitrache',
                 text: 'Marius Mitrache, multiple national champion bodybuilder, explains to us if we can build muscle mass on a plant-based diet.',
                 videoId: 'DWcSzjKpDHY',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '8vw'
+                },
                 word: false
             },
             {
                 name: 'Liviu Deacu',
                 text: 'Liviu Deacu, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 videoId: 'UCQAokuasN8',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '8vw'
+                },
                 word: false
             }
         ]
@@ -68,28 +90,40 @@ const interviews = [
                 name: 'Corina Zugravu',
                 text: 'Corina Zugravu, associate professor for food safety and nutrition, shares with us the number one benefit on a plant based diet. ',
                 videoId: 'Qzmf6FNFRpg',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '7.5vw'
+                },
                 word: false
             },
             {
                 name: 'Gigi Ghinea',
                 text: 'Gigi Ghinea, psychologist and author, shares her opinion on the link between a post-communism country and eating habits.',
                 videoId: 'EyWZvwAA95w',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '7.5vw'
+                },
                 word: false
             },
             {
                 name: 'Doinea Badea',
                 text: 'Doina Badea, the co-founder of the first farm animal sanctuary in Romania, shares her appreciation and love for the kind cuddly giants, the cows. ',
                 videoId: 'KlpWbNeyo_w',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '7.5vw'
+                },
                 word: false
             },
             {
                 name: 'Paul Sandu',
                 text: '',
                 videoId: '',
-                fontSize: '5vw',
+                fontSize: {
+                    browser: '5vw',
+                    mobile: '7.5vw'
+                },
                 word: false
             }
         ]
@@ -101,35 +135,50 @@ const interviews = [
                 name: 'Bogdan Gioara',
                 text: '',
                 videoId: '',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Sorin Dumitru',
                 text: 'Sorin Dumitru, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 videoId: 'ZtCgWHUxLSA',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Anca Georgescu',
                 text: 'Anca Georgescu, director de strategie și comunicare de la TAC medicina veterinara sociala, ne povestește despre câinii singuri și despre cum îi putem ajuta.',
                 videoId: 'htCuv9sLoEA',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Katharina Hani',
                 text: 'Katharina Hani, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 videoId: 'NiA87gSUX_w', 
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Ionut Catalin',
                 text: '',
                 videoId: '',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             }
         ]
@@ -141,28 +190,40 @@ const interviews = [
                 name: 'Nicusor Popa',
                 text: 'Nicusor Popa, founder at Spirit Animals Sanctuary, talks about the true nature of horses.',
                 videoId: '2A1eZqShnyM',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Iulia Morosanu',
                 text: '',
                 videoId: '',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Marisha Zaporojan',
                 text: '',
                 videoId: '',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             },
             {
                 name: 'Stefan Foldi',
                 text: '',
                 videoId: '',
-                fontSize: '4.5vw',
+                fontSize: {
+                    browser: '4.5vw',
+                    mobile: '6.5vw'
+                },
                 word: false
             }
         ]
@@ -219,7 +280,7 @@ export default class Interviews extends Component {
                                         return (
                                             <p 
                                             class={interviewee.word ? style.interviewee__word : style.interviewee__name} 
-                                            style={{fontSize: interviewee.fontSize}}
+                                            style={{fontSize: isMobile ? interviewee.fontSize.mobile : interviewee.fontSize.browser }}
                                             onClick={() => this.handleModal(interviewee)}
                                                 >
                                                 {interviewee.name}
